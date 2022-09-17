0ec8cdf01ec0b0ccc3bd33be6909e659da0911b1
L0Lock <myuline.annatar@gmail.com>
2022-09-17 17:41:10 +0200

    added import function

--- import_syncsketch_comments.py
+++ import_syncsketch_comments.py
@@ -1,10 +1,10 @@
 import bpy
 from bpy.types import Panel, Operator
-from bpy.props import IntProperty
+from bpy.props import IntProperty, StringProperty
 from rna_prop_ui import PropertyPanel
 from bl_ui.utils import PresetPanel
-import csv
-import textwrap
+from bpy_extras.io_utils import ImportHelper
+import csv, textwrap, shutil, os
 
 class CameraButtonsPanel:
     bl_space_type = 'PROPERTIES'
@@ -28,6 +28,44 @@ class ISN_OT_jumpToFrame(Operator):
         bpy.context.scene.frame_set(self.frameJump)
         return {'FINISHED'}
 
+class ISN_OT_import_csv(Operator, ImportHelper):
+    """Imports Syncsketch's notes (Maya greasepencil zip) as camera background sequence."""
+    bl_idname = 'isn.import_csv'
+    bl_label = 'Import Syncsketch Comments'
+    bl_options = {'PRESET', 'UNDO'}
+
+    filename_ext = '.csv'
+    
+    filter_glob: StringProperty(
+        default='*.csv',
+        options={'HIDDEN'}
+    )
+
+    def execute(self, context):
+        csvFile = self.filepath
+        csvName = os.path.basename(csvFile).split('/')[-1]
+        csvPath = os.path.dirname(os.path.abspath(csvFile))
+        blendPath = bpy.path.abspath("//")
+        commentsTarget = blendPath + 'SyncsketchNotes\\' + os.path.splitext(csvName)[0]
+
+        print(f'imported file (full): {csvFile}')
+        print(f'imported file name: {csvName}')
+        print(f'imported file path: {csvPath}')
+        print(f'blend path: {blendPath}')
+        print(f'target notes path: {commentsTarget}')
+
+        shutil.copy2(csvFile, commentsTarget)
+
+        for txt in bpy.data.texts:
+            if txt.name == 'SyncsketchComments':
+                bpy.data.texts.remove(bpy.data.texts["SyncsketchComments"])
+
+        txt = bpy.data.texts.load(csvFile)
+        txt.name = 'SyncsketchComments'
+
+
+        return {'FINISHED'}
+
 class ISN_PT_syncsketch(CameraButtonsPanel, Panel):
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
@@ -41,12 +79,15 @@ class ISN_PT_syncsketch(CameraButtonsPanel, Panel):
 
     def _getCsvList(self):
         
-#        csvFile = open("C:\\Users\\Roikku\\Documents\\Repositories\\import_synkscetch_notes\\Workbench\\test.csv", "r")
+        # csvFile = open("C:\\Users\\Roikku\\Documents\\Repositories\\import_synkscetch_notes\\Workbench\\test.csv", "r")
         # outfile = open("C:\\Users\\Roikku\\Documents\\Repositories\\import_synkscetch_notes\\Workbench\\SynckSketchComments.md", "w")
         quote_character='"'
         delimiter=','
 
-        with open("C:\\Users\\Roikku\\Documents\\Repositories\\import_synkscetch_notes\\Workbench\\test.csv", "r") as csvFile:
+        if bpy.data.texts.get('SyncsketchComments') == None:
+            return None
+
+        with open(bpy.data.texts["SyncsketchComments"].filepath, "r") as csvFile:
             reader = csv.DictReader(csvFile, delimiter=delimiter, quotechar=quote_character, restkey='unrecognized_cols')
             csvList = list()
             for dictionary in reader:
@@ -66,7 +107,7 @@ class ISN_PT_syncsketch(CameraButtonsPanel, Panel):
         # apply scale factor for retina screens etc
         width = int(width / self._dpi_scale())
         # padding to account for some kerning
-#        self.wrap = width/7
+        # self.wrap = width/7
         
         return width
     
@@ -84,6 +125,12 @@ class ISN_PT_syncsketch(CameraButtonsPanel, Panel):
         width = self._char_width()
 
         csvList = self._getCsvList()
+
+        if csvList == None:
+            row=layout.row()
+            box=layout.box()
+            box.scale_y = 1
+            box.label(text=f'No comments to display')
 
         ReviewName = csvList[1]["Review Name"]
         ItemName = csvList[1]["Item Name"]
@@ -126,30 +173,35 @@ class ISN_PT_syncsketch(CameraButtonsPanel, Panel):
             row.alignment = 'LEFT'
             box.label(text='·'*int(width*1.64))
             for text in Comment: 
-#                box.(align = True)
+                # box.(align = True)
                 box.alignment = 'EXPAND'
                 box.label(text=text)
-#            box.label(text=f'{Comment}')
+            # box.label(text=f'{Comment}')
             # print(f'| {NoteFrame} | *{Author}*:<br/>{Comment} |',file=outfile)
         # csvFile.close()
         # outfile.close()
         
 classes = (
     ISN_OT_jumpToFrame,
+    ISN_OT_import_csv,
     ISN_PT_syncsketch,
 )
+def menu_func_import(self, context):
+    self.layout.operator(ISN_OT_import_csv.bl_idname, text="Syncsketch Comments", icon='FILE_TEXT')
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
     bpy.types.Scene.frameJump = IntProperty()
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
     del bpy.types.Scene.frameJump
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
 
 
 if __name__ == "__main__":  # only for live edit.
+    # unregister()
     register()
-#    unregister()
